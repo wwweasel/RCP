378b54a885d279a32034c38dcc48c4c800ab62b9
w <weasel@tutamail.com>
2020-03-27 11:33:56 +0100

    init_commit

---
+++ Project/src/main/java/de/wwweasel/RCP/RcpApplication.java
@@ -0,0 +1,13 @@
+package de.wwweasel.RCP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RcpApplication.class, args);
+	}
+
+}
---
+++ Project/src/test/java/de/wwweasel/RCP/RcpApplicationTests.java
@@ -0,0 +1,13 @@
+package de.wwweasel.RCP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
